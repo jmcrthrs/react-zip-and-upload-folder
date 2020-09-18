a24da121382bc24a99c7c2d06202f7c7bc7544e9
Jamie <jamie.carruthers@live.com>
2020-09-18 16:12:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+import JSZip from "jszip";
+import throttle from "lodash.throttle";
+
+export default function App() {
+  const inputRef = useRef(null);
+  const [progress, setProgress] = useState(-1);
+  const [files, setFiles] = useState([]);
+  const [zippedFiles, setZippedFiles] = useState([]);
+
+  const onZipUpdate = (metadata) => {
+    setProgress(metadata.percent);
+    console.log(zippedFiles);
+    setZippedFiles([...zippedFiles, metadata.currentFile]);
+    console.log("progression: " + metadata.percent.toFixed(2) + " %");
+    if (metadata.currentFile) {
+      console.log("current file = " + metadata.currentFile);
+    }
+  };
+  const throttledZipUpdate = throttle(onZipUpdate, 500);
+
+  const onZip = () => {
+    const zip = new JSZip();
+    const files = Array.from(inputRef.current.files);
+
+    files.forEach((file) => {
+      zip.file(file.webkitRelativePath, file);
+    });
+    zip
+      .generateAsync({ type: "blob" }, throttledZipUpdate)
+      .then(function (content) {
+        const formData = new FormData();
+        formData.append("folderzip", content);
+        console.log("ready to send to server", content);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  return (
+    <div className="App">
+      <h1>Folder upload</h1>
+      <h2>Select a folder to send to the server</h2>
+      <input
+        ref={inputRef}
+        type="file"
+        webkitdirectory="true"
+        multiple
+        onChange={() => {
+          setFiles(Array.from(inputRef.current.files));
+        }}
+      />
+      {files.length && (
+        <div>
+          <div>
+            <button onClick={onZip}>zip {files.length} files</button>
+          </div>
+          <progress max="100" value={progress}>
+            {progress?.toFixed(2)}%{" "}
+          </progress>
+          <h3>Zipped</h3>
+          {zippedFiles.length}
+          {zippedFiles.map((file) => (
+            <div key={file}>{file}</div>
+          ))}
+          <h3>Selected Files</h3>
+          {files.map((file) => (
+            <div key={file.webkitRelativePath}>{file.webkitRelativePath}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
