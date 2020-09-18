6d56e03bad68e2d914722d7a85398ca336738920
Jamie <jamie.carruthers@live.com>
2020-09-18 16:17:02 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ export default function App() {
   const inputRef = useRef(null);
   const [progress, setProgress] = useState(-1);
   const [files, setFiles] = useState([]);
-  const [zippedFiles, setZippedFiles] = useState([]);
 
   const onZipUpdate = (metadata) => {
     setProgress(metadata.percent);
-    console.log(zippedFiles);
-    setZippedFiles([...zippedFiles, metadata.currentFile]);
     console.log("progression: " + metadata.percent.toFixed(2) + " %");
     if (metadata.currentFile) {
       console.log("current file = " + metadata.currentFile);
@@ -57,11 +54,6 @@ export default function App() {
           <progress max="100" value={progress}>
             {progress?.toFixed(2)}%{" "}
           </progress>
-          <h3>Zipped</h3>
-          {zippedFiles.length}
-          {zippedFiles.map((file) => (
-            <div key={file}>{file}</div>
-          ))}
           <h3>Selected Files</h3>
           {files.map((file) => (
             <div key={file.webkitRelativePath}>{file.webkitRelativePath}</div>
